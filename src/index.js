3e179b9a10bc8b634c2bae483d11ff9708ea7c5e
jw2202 <jinwoopark2202@gmail.com>
2025-03-27 09:32:13 +0900

    minimax algorithm

--- src/index.js
+++ src/index.js
@@ -38,6 +38,54 @@ function evaluateBoard() {
   return score;
 }
 
+function miniMax(depth) {
+  if (game.isCheckmate()) {
+    return { 
+      score: game.turn() == 'w' ? 999999999999 : -999999999999
+    }
+  }
+
+  if (game.isDraw()) {
+    return {
+      score: 0
+    }
+  }
+
+  if (depth == 0) {
+    return {
+      score: evaluateBoard()
+    }
+  }
+
+  let moves = game.moves();
+
+  let evaluated = [];
+  for (let i = 0; i < moves.length; i++) {
+    var move = moves[i];
+    game.move(move);
+    var result = miniMax(depth - 1);
+    game.undo();
+    evaluated.push({ move: move, score: result.score });
+  }
+
+  let best = evaluated[0];
+  if (game.turn() == 'w') {
+    for (let i = 0; i < evaluated.length; i++) {
+      if (evaluated[i].score > best.score) {
+        best = evaluated[i];
+      }
+    }
+  } else {
+    for (let i = 0; i < evaluated.length; i++) {
+      if (evaluated[i].score < best.score) {
+        best = evaluated[i];
+      }
+    }
+  }
+
+  return best;
+}
+
 function onDrop (source, target) {
   try {
     // see if the move is legal
@@ -108,8 +156,11 @@ function updateStatus() {
   $pgn.html(game.pgn());
 
   if (moveColor === 'Black' && !game.isGameOver()) {
-    var validMoves = game.moves();
-    game.move(validMoves[Math.floor((Math.random() * 1000) % validMoves.length)]);
+    // var validMoves = game.moves();
+    // game.move(validMoves[Math.floor((Math.random() * 1000) % validMoves.length)]);
+    var bestMove = miniMax(3);
+    game.move(bestMove.move);
+
     updateStatus();
   }
 }
